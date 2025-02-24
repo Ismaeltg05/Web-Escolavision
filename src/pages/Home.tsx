4efbae6176df188b9e301b6ac5a4fe688b132ced
Adrián Ruiz Sánchez <156907273+adrianrs2510@users.noreply.github.com>
2025-02-24 11:56:19 +0100

    Logo Escolavision y Update Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
     return (
-        <div className="p-8 max-w-4xl mx-auto bg-white shadow-lg rounded-lg">
+        <div className="p-8 max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
             <br />
             <h1 className="text-4xl font-bold text-center text-blue-600 mb-8">Encuentra tu Camino Académico y Profesional</h1>
             
