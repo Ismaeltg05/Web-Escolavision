076f869b87dec596bd368c745eb4996b978d020a
Ismael Torres González <ismaeltorresgon@gmail.com>
2025-02-26 12:06:23 +0100

    img on about fixed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,18 @@
+import { HeadingIcon } from "lucide-react";
+
 function About() {
   return (
     <div className="flex flex-col items-center p-6">
       <h1 className="text-3xl font-bold mb-6">Sobre Nosotros</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="bg-white p-6 rounded-2xl shadow-lg text-center">
-          <img src="/assets/isma.jpeg" alt="Ismael Torres" className="w-12 h-24 mx-auto rounded-full mb-4" />
+          <img src="./assets/isma.jpeg" alt="Ismael Torres" className="w-24 h-24 mx-auto rounded-full mb-4" style={{maxHeight: "256px", maxWidth: "256px", }} />
           <h2 className="text-xl font-semibold">Ismael Torres</h2>
           <p className="text-gray-600"></p>
           <p className="text-gray-500">Correo: ismaeltg@escolavision.com</p>
         </div>
         <div className="bg-white p-6 rounded-2xl shadow-lg text-center">
-          <img src="/assets/adri.jpeg" alt="Adrián Ruiz" className="w-24 h-24 mx-auto rounded-full mb-4" />
+          <img src="./assets/adri.jpeg" alt="Adrián Ruiz" className="w-24 h-24 mx-auto rounded-full mb-4" style={{maxHeight: "256px", maxWidth: "256px"}}/>
           <h2 className="text-xl font-semibold">Adrián Ruiz</h2>
           <p className="text-gray-600"></p>
           <p className="text-gray-500">Correo: adrianrs@escolavision.com</p>
